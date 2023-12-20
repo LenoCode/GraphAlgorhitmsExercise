5c8bb24a987a28e4f6a55d46b766d3c6b1ea813c
leno <leno.application@gmail.com>
2023-12-20 20:56:58 +0100

    Added first phase of learning

---
+++ include/interface/graph_adt.h
@@ -0,0 +1,28 @@
+#include <vector>
+
+struct Edge{
+    int v,w;
+    Edge(int v = -1,int w = -1):v(v),w(w){}
+};
+
+
+//he simplest interface that supports the basic graph-processing operations that we wish to consider
+class Graph{
+
+    private:
+        //Implementation-dependet code
+
+    public:
+        virtual ~Graph(){};
+
+        virtual int V() const = 0;
+        virtual int E() const = 0;
+
+        virtual bool directed() const = 0;
+
+        virtual int insert(Edge) = 0;
+        virtual int remove(Edge) = 0;
+
+        virtual bool edge(int,int) = 0;
+
+};
